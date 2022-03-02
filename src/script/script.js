6e5df43425c700c4010baa016387c0e346072519
Leandro José de Oliveira Júnior <leandro-nim@hotmail.com>
2022-03-02 20:02:49 -0300

    Icon changed

--- src/script/script.js
+++ src/script/script.js
@@ -10,7 +10,9 @@ const includeNumbersToggle = document.querySelector("#input-numbers")
 const includeUppercaseToggle = document.querySelector("#input-uppercase")
 const includeLowercaseToggle = document.querySelector("#input-lowercase")
 const excludeSimilarToggle = document.querySelector("#input-exclude-similar")
-const excludeAmbiguousToggle = document.querySelector("#input-exclude-ambiguous")
+const excludeAmbiguousToggle = document.querySelector(
+	"#input-exclude-ambiguous"
+)
 const generateButton = document.querySelector("#button-generate")
 const copyButton = document.querySelector("#button-copy")
 const resultTextarea = document.querySelector("#textarea-result")
@@ -21,7 +23,26 @@ const originalNumbers = "1234567890"
 const originalUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const originalLowercase = "abcdefghijklmnopqrstuvwxyz"
 const similar = ["i", "l", "1", "L", "o", "O", "0"]
-const ambiguous = ["[","]","{","}","(",")","/","\\","'","\"","`","~",",",";",":",".","<",">",]
+const ambiguous = [
+	"[",
+	"]",
+	"{",
+	"}",
+	"(",
+	")",
+	"/",
+	"\\",
+	"'",
+	'"',
+	"`",
+	"~",
+	",",
+	";",
+	":",
+	".",
+	"<",
+	">",
+]
 let symbols = "`~!@#$%^&*()-_=+[{]}\\;:',<.>/?"
 let numbers = "1234567890"
 let lettersUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -64,82 +85,81 @@ initialSetup()
 
 //Sets the input and checkboxes of the interface with the default values
 function initialSetup() {
-
 	//Amount
 	amountInput.value = 16
 	passSafetyText.innerText = "STRONG"
-	jsonStorage.get("dpgLocalStorageAmount", function (error, data){
+	jsonStorage.get("dpgLocalStorageAmount", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.amount
-			if(i != undefined) amountInput.value = i
+			if (i != undefined) amountInput.value = i
 			else amountInput.value = 16
 			changePassSafetyText()
 		}
 	})
-	
+
 	//Include Symbols
 	includeSymbolsToggle.checked = true
-	jsonStorage.get("dpgLocalStorageIncludeSymbols", function (error, data){
+	jsonStorage.get("dpgLocalStorageIncludeSymbols", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.includeSymbols
-			if(i != undefined) includeSymbolsToggle.checked = i
+			if (i != undefined) includeSymbolsToggle.checked = i
 			else includeSymbolsToggle.checked = true
 		}
 	})
 
 	//Include Numbers
 	includeNumbersToggle.checked = true
-	jsonStorage.get("dpgLocalStorageIncludeNumbers", function (error, data){
+	jsonStorage.get("dpgLocalStorageIncludeNumbers", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.includeNumbers
-			if(i != undefined) includeNumbersToggle.checked = i
+			if (i != undefined) includeNumbersToggle.checked = i
 			else includeNumbersToggle.checked = true
 		}
 	})
 
 	//Include Uppercase
 	includeUppercaseToggle.checked = true
-	jsonStorage.get("dpgLocalStorageIncludeUppercase", function (error, data){
+	jsonStorage.get("dpgLocalStorageIncludeUppercase", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.includeUppercase
-			if(i != undefined) includeUppercaseToggle.checked = i
+			if (i != undefined) includeUppercaseToggle.checked = i
 			else includeUppercaseToggle.checked = true
 		}
 	})
 
 	//Include Lowercase
 	includeLowercaseToggle.checked = true
-	jsonStorage.get("dpgLocalStorageIncludeLowercase", function (error, data){
+	jsonStorage.get("dpgLocalStorageIncludeLowercase", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.includeLowercase
-			if(i != undefined) includeLowercaseToggle.checked = i
+			if (i != undefined) includeLowercaseToggle.checked = i
 			else includeLowercaseToggle.checked = true
 		}
 	})
 
 	//Exclude Similar
 	excludeSimilarToggle.checked = true
-	jsonStorage.get("dpgLocalStorageExcludeSimilar", function (error, data){
+	jsonStorage.get("dpgLocalStorageExcludeSimilar", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.excludeSimilar
-			if(i != undefined) excludeSimilarToggle.checked = i
+			if (i != undefined) excludeSimilarToggle.checked = i
 			else excludeSimilarToggle.checked = true
 		}
 	})
 
 	//Exclude Ambiguous
 	excludeAmbiguousToggle.checked = true
-	jsonStorage.get("dpgLocalStorageExcludeAmbiguous", function (error, data){
+	jsonStorage.get("dpgLocalStorageExcludeAmbiguous", function (error, data) {
 		if (error) throw error
 		if (data) {
 			let i = data.excludeAmbiguous
-			if(i != undefined) excludeAmbiguousToggle.checked = i
+			if (i != undefined) excludeAmbiguousToggle.checked = i
 			else excludeAmbiguousToggle.checked = true
 		}
 	})
@@ -228,12 +248,11 @@ function getValuesFromInterface() {
 
 //create the events when the actions are performed in the GUI
 generateButton.addEventListener("click", () => {
-
 	//Updates the parameters
 	getValuesFromInterface()
 
 	//Blocks the Generate Button from working in case no category is selected
-	if(chosenCategories.length == 0) return
+	if (chosenCategories.length == 0) return
 
 	generatePass()
 })
@@ -248,38 +267,48 @@ amountInput.addEventListener("change", () => {
 	//Stores the new value
 	//amount = Number(amountInput.value)
 	amount = amountInput.value
-	
+
 	changePassSafetyText()
 
 	//Saves the amount locally
-	jsonStorage.set("dpgLocalStorageAmount", { amount : Number(amount) })
-
+	jsonStorage.set("dpgLocalStorageAmount", { amount: Number(amount) })
 })
 includeSymbolsToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageIncludeSymbols", { includeSymbols: includeSymbolsToggle.checked })
+	jsonStorage.set("dpgLocalStorageIncludeSymbols", {
+		includeSymbols: includeSymbolsToggle.checked,
+	})
 })
 includeNumbersToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageIncludeNumbers", { includeNumbers: includeNumbersToggle.checked })
+	jsonStorage.set("dpgLocalStorageIncludeNumbers", {
+		includeNumbers: includeNumbersToggle.checked,
+	})
 })
 includeUppercaseToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageIncludeUppercase", { includeUppercase: includeUppercaseToggle.checked })
+	jsonStorage.set("dpgLocalStorageIncludeUppercase", {
+		includeUppercase: includeUppercaseToggle.checked,
+	})
 })
 includeLowercaseToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageIncludeLowercase", { includeLowercase: includeLowercaseToggle.checked })
+	jsonStorage.set("dpgLocalStorageIncludeLowercase", {
+		includeLowercase: includeLowercaseToggle.checked,
+	})
 })
 excludeSimilarToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageExcludeSimilar", { excludeSimilar: excludeSimilarToggle.checked })
+	jsonStorage.set("dpgLocalStorageExcludeSimilar", {
+		excludeSimilar: excludeSimilarToggle.checked,
+	})
 })
 excludeAmbiguousToggle.addEventListener("change", () => {
 	//Saves locally
-	jsonStorage.set("dpgLocalStorageExcludeAmbiguous", { excludeAmbiguous: excludeAmbiguousToggle.checked })
+	jsonStorage.set("dpgLocalStorageExcludeAmbiguous", {
+		excludeAmbiguous: excludeAmbiguousToggle.checked,
+	})
 })
-
 
 window.myAPI.onQuickGeneration(() => {
 	generatePass()
@@ -288,7 +317,6 @@ window.myAPI.onQuickGeneration(() => {
 
 //Main function to generate the random password based on the parameters
 function generatePass() {
-
 	getValuesFromInterface()
 
 	//Erases the password in memory
@@ -300,28 +328,28 @@ function generatePass() {
 	//I place one char of each categorie on the beginning of the password to make sure that every password will have at least one of each chosen categorie
 	for (var i = 0; i < chosenCategories.length; i++) {
 		switch (chosenCategories[i]) {
-		case "symbols":
-			password += symbols.charAt(
-				Math.floor(Math.random() * symbols.length)
-			)
-			break
-		case "numbers":
-			password += numbers.charAt(
-				Math.floor(Math.random() * numbers.length)
-			)
-			break
-		case "uppercase":
-			password += lettersUppercase.charAt(
-				Math.floor(Math.random() * lettersUppercase.length)
-			)
-			break
-		case "lowercase":
-			password += lettersLowercase.charAt(
-				Math.floor(Math.random() * lettersLowercase.length)
-			)
-			break
-		default:
-			console.log("No categories selected")
+			case "symbols":
+				password += symbols.charAt(
+					Math.floor(Math.random() * symbols.length)
+				)
+				break
+			case "numbers":
+				password += numbers.charAt(
+					Math.floor(Math.random() * numbers.length)
+				)
+				break
+			case "uppercase":
+				password += lettersUppercase.charAt(
+					Math.floor(Math.random() * lettersUppercase.length)
+				)
+				break
+			case "lowercase":
+				password += lettersLowercase.charAt(
+					Math.floor(Math.random() * lettersLowercase.length)
+				)
+				break
+			default:
+				console.log("No categories selected")
 		}
 	}
 
